55c3c56c5794fa611d965f26cc4df92c147d2604
nousvang <80865244+nousvang@users.noreply.github.com>
2021-05-07 21:50:45 -0500

    created rows for predicted days forecast

--- src/index.js
+++ src/index.js
@@ -21,6 +21,33 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+    <div class="col-2">
+    <div class="weather-forecast-day">${day}</div>
+    <img
+     src="http://openweathermap.org/img/wn/50d@2x.png"
+       width="40"
+       />
+    <div class="weather-forecast-temps">
+     <span class="weather-forecast-temp-min">12°</span>
+     <span class="weather-forecast-temp-max">18°</span>
+     </div>
+     </div>
+    `;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -87,3 +114,5 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", showCelsiusTemp);
 
 search("New York");
+
+displayForecast();
